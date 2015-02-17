20c58fe811863e7b86a34b363f14c0f7b11dd518
John <jjkalafut@gmail.com>
2015-02-17 13:27:20 -0600

    Nathan's first changes

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 ﻿var view = this;
 var projectorModeWindow;
 var modecount = 0;
-var texttopass=new Array();
+var texttopass= new Array();
 var propValue = new Array();
-var teams=new Array();
+var teams = new Array();
+var passedInformation = new Array(); //Hosts the information to be passed to the projector view
 
 //view.projectorModeOn opens projector mode.
 view.projectorModeOn = function () {
     modecount++;
     var projmode = document.getElementById("projectormode");
     if (modecount % 2 == 1) {
-        projmode.nodeValue = ("Projector Mode is On");
+        projmode.value = ("Projector Mode is On");
         projectorModeWindow = window.open("ProjectorDisplay.html", "_blank", "fullscreen=0,toolbar=0,location=0,menubar=1,directories=0,scrollbars=0,resizable=1");
     } else {
         projectorModeWindow.close();
-        projmode.nodeValue = ("Projector Mode is Off");
+        projmode.value = ("Projector Mode is Off");
         modecount = 0;
     }
 };
@@ -55,106 +56,90 @@ var obj = new Object();
 		count++;
 	}
 	
-	for(var i=0; i<propValue.length;i++)
-	{
+	for(var i=0; i<propValue.length;i++){
 		console.log(propValue[i].results);
 	}
-	//obj = JSON.parse(json);
-	for(var j=0;j<count;j++)
-	{
-
-		if(propValue[j].results===null)
-		{
+	for(var j=0;j<count;j++){
+		if(propValue[j].results===null){
 		}
-		else
-		{
+		else{
 			teams=propValue[j].results;
 		}
-		//alert(eventname);
-		
-		//rearranges score array from lowest to highest
-		teams.sort(function(a,b) { return parseFloat(a.score) - parseFloat(b.score) } );
-		
-		for(var i=0;i<teams.length;i++)
-		{
+		//rearranges score array from highest to lowest
+		teams.sort(function(a,b) { return parseFloat(b.score) - parseFloat(a.score) } );
+		for(var i=0;i<teams.length;i++){
 			var tempscore="No score"
 			if (teams[i].score!==undefined)
 			{
 				tempscore=teams[i].score;
-				//teams[i].score='null';
 			}
 			if (teams[i].score===null)
 			{
 				teams[i].score='';
 			}
 			texttopass[i]=(teams[i].team + ": "+ tempscore);
-			//alert(texttopass[i]);
-			//if (teams[i].students!==undefined)         <-------this will be moved later?
-			//{
-				//alert(teams[i].students);
-			//}
 		}
 	}
 	addTable();
 };
 
-function addTable() {
-       
+function addTable() {   // Adds the first table
         var myTableDiv = document.getElementById("displayTable")
         var table = document.createElement('TABLE')
         var tableBody = document.createElement('TBODY')
-
-        //table.border = '1'
         table.appendChild(tableBody);
-
         //dynamic table row and column creation
 		for (i = 0; i < propValue.length; i++) {
 			var tr = document.createElement('TR');
 			var td = document.createElement('TD');
 			var td_btn = document.createElement('Button');
 			td_btn.index=i;
-			td_btn.onclick = function() {
-			if(modecount==1)
-			{
-				alert(this.index);
-			}
-			else
-			{
+			td_btn.onclick = function() {		
 				tableButtons(this.index);
-			}
 			};
 			td.appendChild(document.createTextNode(propValue[i].name));
-			td_btn.appendChild(document.createTextNode('Display Event'));
+			td_btn.appendChild(document.createTextNode('Expand Teams'));
 			tr.appendChild(td);
 			tr.appendChild(td_btn);
 			tableBody.appendChild(tr);
 		}
     myTableDiv.appendChild(table);
-	document.body.appendChild(table);
 };
 
-function tableButtons(id) {
-	var teamString = null;
-	var teamScore = null;
-	var currentEventString = null;
-	var currentEvent = document.getElementById("teamInformation");
-	currentEvent.innerHTML = " ";
-	if(propValue[id].results === null)
-	{
-		teamScore = 0;
-		currentEventString = "Undefined";
-		currentEvent.innerHTML = currentEventString+": "+teamScore;
-	}
-	else
-	{
+function tableButtons(id) {   //Adds the scores and team information when the Expand Teams buttons are clicked
+		var myTableDiv = document.getElementById("teamInformation")
+		while (myTableDiv.hasChildNodes()) {
+			myTableDiv.removeChild(myTableDiv.lastChild);
+		} 
+        var table = document.createElement('TABLE')
+        var tableBody = document.createElement('TBODY')
+		var teamScore = null;
+		var currentEventString = null;
+        table.appendChild(tableBody);
 		var teamString = propValue[id].results;
-		for(i=0;i<teamString.length;i++)
-		{
+        //dynamic table row and column creation
+		for (i = 0; i < teamString.length; i++) {
+					var tr = document.createElement('TR');
+			var td = document.createElement('TD');
+			var td_btn = document.createElement('Button');
+			td_btn.index=i;
+			td_btn.onclick = function() {
+				getPassableInformation(this.index);
+			};
 			teamScore = teamString[i].score;
 			currentEventString = (teamString[i].team);
-			//currentEvent.innerHTML = currentEventString+": "+teamScore;
-			currentEvent.appendChild(document.createTextNode(currentEventString+": "+teamScore));
-			currentEvent.appendChild(document.createElement("br"));
+			td.appendChild(document.createTextNode(currentEventString+": "+teamScore));
+			td_btn.appendChild(document.createTextNode('Display Event'));
+			passedInformation[i]=currentEventString+": "+teamScore;
+			tr.appendChild(td);
+			tr.appendChild(td_btn);
+			tableBody.appendChild(tr);
 		}
-	}
+    myTableDiv.appendChild(table);
 };
+function getPassableInformation(id) {
+		alert(passedInformation[id]);
+		//return passedInformation[id];
+	}
+
+

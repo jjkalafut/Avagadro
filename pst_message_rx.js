31e63dc0c07278411c7bb8df74976599616d9a9c
Jerry Pendleton III <jerry_pendleton@yahoo.com>
2015-04-13 22:00:06 -0500

    Display update
    I added the Title to the display that scales with window size. In
    addition, the title will hide itself whenever the results are shown and
    reappear when there aren't any.

--- pst_message_rx.js
+++ pst_message_rx.js
@@ -12,11 +12,11 @@ function listener(event){
 	var pos = str.search("event:");
 	if( pos >= 0){
 		document.getElementById("Title").innerHTML = str.substr(pos+7);
-		
 		return;
 	}
 	pos = str.search("Rank: ");
 	if(pos >= 0 ){
+                document.getElementById("logoBig").style.display = "none";
 		placeRank( str.charAt(pos+6), str.substr(pos+9));
 		return;
 	}
@@ -34,6 +34,7 @@ function listener(event){
 			teams[i].innerHTML="";
 			students[i].innerHTML="";
 		}
+                document.getElementById("logoBig").style.display = "initial";
 	}
 }
 
